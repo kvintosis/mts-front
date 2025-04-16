05f1814b6ed75217a101326c0d1729e04c1bf37e
daryafrllva <dariyf995@gmail.com>
2025-04-16 12:34:44 +0700

    просто кнопку добавили на глав стр

--- mts-app/src/App.jsx
+++ mts-app/src/App.jsx
@@ -4,13 +4,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Header from './components/Header';
+import HomePage from './components/HomePage';
 
 function App() {
     return (
         <Router>
             <Header />
             <div style={{ paddingTop: '147px' }}>
-
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                </Routes>
             </div>
         </Router>
     );
---
+++ mts-app/src/components/HomePage.jsx
@@ -0,0 +1,15 @@
+
+
+import React from 'react';
+
+function HomePage() {
+
+    return (
+        <div className="home__container">
+            <div className="home__btn-container">
+                <button className="home__btn">Конвертировать</button>
+            </div>
+        </div>
+    )
+}
+export default HomePage;
