3efc1dddeed3137d35841a2bdb0837f14ad6c6e1
daryafrllva <dariyf995@gmail.com>
2025-04-16 17:05:07 +0700

    update

--- mts-app/src/components/HomePage.jsx
+++ mts-app/src/components/HomePage.jsx
@@ -16,24 +16,23 @@ function HomePage() {
             <div className="home__container">
                 <h1>Конвертировать изображения в PDF</h1>
                 <h2>что-то написано ....</h2>
-                <div className="home__container-description">
-                    <i>Онлайн</i>
-                    <i>Беспалтно</i>
-                    <i>Быстро</i>
+
+                <div className="home__features">
+                    <span className="feature-badge">✓ Быстро </span>
+                    <span className="feature-badge">✓ Онлайн </span>
+                    <span className="feature-badge">✓ Безопасно </span>
                 </div>
 
                 <div className="home__form">
 
                         <div className="home__btn-container">
                             <button className="home__btn">добавить файл</button>
+                            <button className="home__btn" onClick={handleRedirect}>Конвертировать</button>
                         </div>
                 </div>
-        <div className="home__container">
-            <div className="home__btn-container">
-                <button className="home__btn">добавить файл</button>
-                <button className="home__btn" onClick={handleRedirect}>Конвертировать</button>
+
             </div>
         </div>
     )
-}
+};
 export default HomePage;
