9085715cdb3d7f5dd9100e2fe71282981e0d0e58
daryafrllva <dariyf995@gmail.com>
2025-04-17 14:22:20 +0700

    //

--- mts-app/src/components/HomePage.jsx
+++ mts-app/src/components/HomePage.jsx
@@ -14,7 +14,7 @@ function HomePage() {
     return (
         <div className="home">
             <h1>Конвертировать изображения в PDF</h1>
-            <h2>что-то написано ....</h2>
+            <h2>НЕ КОНВЕРТЕР, А КОНВЕРТИЩЕ</h2>
 
             <div className="home__features">
                 <span className="feature-badge">✓ Быстро </span>
@@ -23,14 +23,22 @@ function HomePage() {
             </div>
 
             <div className="home__container">
-                <div className="home__form">
 
-                        <div className="home__btn-container">
-                            <button className="home__btn">добавить файл</button>
-                            <button className="home__btn" onClick={handleRedirect}>Конвертировать</button>
-                        </div>
+                    <div className="home__btn-container">
+                        <button className="home__btn">добавить файл</button>
+                        <button className="home__btn" onClick={handleRedirect}>Конвертировать</button>
+                    </div>
+
+            </div>
+
+            <div className="home__container-info-block">
+
+                <h1 className="home__info"> Информация </h1>
+
+                <div className="home__container-info">
+
+
                 </div>
-
             </div>
         </div>
     )
