98df0cb3953553ce5d126e233a497fd69889f38f
daryafrllva <dariyf995@gmail.com>
2025-04-18 00:31:04 +0700

    стр профиля

--- mts-app/src/pages/Profile.jsx
+++ mts-app/src/pages/Profile.jsx
@@ -1,6 +1,40 @@
-import React from 'react';
 
-function Profile(){
+
+
+
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Profile() {
+    const navigate = useNavigate();
+
+    const handleRedirect = () => {
+        navigate('/redactor');
+    };
+
+    return (
+        <div className="profile" style={{ paddingTop: '147px' }}>
+            <div className="profile__poisk">
+                <h1>Поиск</h1>
+
+                <div className="search-container">
+                    <input
+                        type="text"
+                        className="search-input"
+                        placeholder="Название документа"
+                        aria-label="Поиск документов"
+                        onChange={handleRedirect}
+                    />
+                    <button className="search-btn" type="submit" aria-label="Найти"></button>
+                </div>
+            </div>
+            <div className="profile__file">
+                <div className="profile__file-case">
+                    <button className="profile__file-case-btn" onClick={handleRedirect}>Редактировать</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Profile;
